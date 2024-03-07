69239529703cd613e090f2dd5bcc13b0cda8a30d
brenddamachado <brenda16machado.bm@gmail.com>
2024-03-07 18:49:37 -0300

    for no js

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+// peagando as informações
 let requesty = new XMLHttpRequest()
-requesty.open("GET","https://bobsburgers-api.herokuapp.com/characters/?limit=9&skip=265")
+requesty.open("GET", "https://bobsburgers-api.herokuapp.com/characters/?limit=9&skip=265")
 requesty.send()
-requesty.onload = function() {
-    let conversor = JSON.parse(requesty.response) // dados da api
-    console.log(conversor)
+requesty.onload = function () {
+    let personagens = JSON.parse(requesty.response) // array de personagens
 
+    const charactersContainer = document.getElementById('characters-container');
+
+    for (let i = 0; i < personagens.length; i++) {
+        const characterDiv = document.createElement('div');
+        const imgElement = document.createElement('img');
+        const nameDiv = document.createElement('div');
+        const genderDiv = document.createElement('div');
+
+        // Atribuir a URL da imagem ao atributo src
+        imgElement.src = personagens[i].image;
+
+        // Adicionar a imagem à div
+        characterDiv.appendChild(imgElement);
+
+        // Adicionar nome e gênero às divs
+        nameDiv.innerText = personagens[i].name;
+        genderDiv.innerText = personagens[i].gender;
+
+        // Adicionar as divs ao container no HTML
+        characterDiv.appendChild(nameDiv);
+        characterDiv.appendChild(genderDiv);
+        charactersContainer.appendChild(characterDiv);
+    }
 }
