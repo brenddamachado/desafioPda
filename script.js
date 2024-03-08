e774fc70ccb2d3cc4c9cd25145ee1829c2440370
brenddamachado <brenda16machado.bm@gmail.com>
2024-03-08 15:23:34 -0300

    tentando aparecer as info

--- script.js
+++ script.js
@@ -4,28 +4,11 @@ requesty.open("GET", "https://bobsburgers-api.herokuapp.com/characters/?limit=9&
 requesty.send()
 requesty.onload = function () {
     let personagens = JSON.parse(requesty.response) // array de personagens
+const div1=document.getElementById("img")
+const div2=document.getElementById("nome")
+const div3=document.getElementById("occupation")
 
-    const charactersContainer = document.getElementById('characters-container');
-
-    for (let i = 0; i < personagens.length; i++) {
-        const characterDiv = document.createElement('div');
-        const imgElement = document.createElement('img');
-        const nameDiv = document.createElement('div');
-        const genderDiv = document.createElement('div');
-
-        // Atribuir a URL da imagem ao atributo src
-        imgElement.src = personagens[i].image;
-
-        // Adicionar a imagem à div
-        characterDiv.appendChild(imgElement);
-
-        // Adicionar nome e gênero às divs
-        nameDiv.innerText = personagens[i].name;
-        genderDiv.innerText = personagens[i].gender;
-
-        // Adicionar as divs ao container no HTML
-        characterDiv.appendChild(nameDiv);
-        characterDiv.appendChild(genderDiv);
-        charactersContainer.appendChild(characterDiv);
-    }
+div1.innerText = personagens.image
+    div2.innerText = personagens.name
+    div3.innerText = personagens.occupation
 }
