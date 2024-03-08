424cb0793769ed51259fac1c4ed58942d6b7628a
brenddamachado <brenda16machado.bm@gmail.com>
2024-03-08 15:46:45 -0300

    cards feito

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-// peagando as informações
-let requesty = new XMLHttpRequest()
-requesty.open("GET", "https://bobsburgers-api.herokuapp.com/characters/?limit=9&skip=265")
-requesty.send()
-requesty.onload = function () {
-    let personagens = JSON.parse(requesty.response) // array de personagens
-const div1=document.getElementById("img")
-const div2=document.getElementById("nome")
-const div3=document.getElementById("occupation")
+let requesty = new XMLHttpRequest();
+requesty.open("GET", "https://bobsburgers-api.herokuapp.com/characters/?limit=9&skip=265");
+requesty.send();
 
-div1.innerText = personagens.image
-    div2.innerText = personagens.name
-    div3.innerText = personagens.occupation
+requesty.onload = function () {
+    let personagens = JSON.parse(requesty.response); // array de personagens
+
+    const container = document.getElementById("container"); // Container para os cards
+
+    // Percorrer todos os personagens e criar um card para cada um
+    for (let i = 0; i < personagens.length; i++) {
+        const personagem = personagens[i];
+
+        // Criar um card para o personagem
+        const card = document.createElement("div");
+        card.classList.add("card"); // Adicionar uma classe para estilização CSS (pode ajustar conforme necessário)
+
+        // Criar elemento de imagem e adicionar ao card
+        const imgElement = document.createElement("img");
+        imgElement.src = personagem.image;
+        card.appendChild(imgElement);
+
+        // Criar elemento para o nome e adicionar ao card
+        const nomeElement = document.createElement("div");
+        nomeElement.innerText = personagem.name;
+        card.appendChild(nomeElement);
+
+        // Criar elemento para a ocupação e adicionar ao card
+        const occupationElement = document.createElement("div");
+        occupationElement.innerText = personagem.occupation;
+        card.appendChild(occupationElement);
+
+        // Adicionar o card ao container
+        container.appendChild(card);
+    }
 }
